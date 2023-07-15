8dd83a3ed18c1ff22d817bf2358ff7d6cfc5efd4
Idk0712 <132455294+Idk0712@users.noreply.github.com>
2023-07-15 18:08:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ const things = {
     ["numbers"]: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
     ["symbols"]: ["$", "<", ">", "^", "*", "~", "`", "ù", "%", "!", "?", ";", ",", "¤", "£", "_", "-", "/", "&", "#", `"`, `'`, "{", "}", "[", "]", "@", "ç", "(", ")", "!", "§"]
 };
-//pls refresh
 password.addEventListener("keydown", dodgeWriting);
 function dodgeWriting(e){
     e.preventDefault();
